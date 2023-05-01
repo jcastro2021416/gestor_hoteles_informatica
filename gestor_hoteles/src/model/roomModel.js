0fcec9c1643d625fb7ba25dbad7c6de1180e9f22
castro2004 <jc300159@gmail.com>
2023-05-01 11:06:52 -0600

    RoomSchema
    Se cambio de number por numberRoom en el archivo roomModel.js

--- gestor_hoteles/src/model/roomModel.js
+++ gestor_hoteles/src/model/roomModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const RoomSchema = new Schema({
-    number: {
+    numberRoom: {
         type: Number,
         required: true,
     },
