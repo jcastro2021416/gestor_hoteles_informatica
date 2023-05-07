1ae8ac1c440fac6e9039c2545f48b28bd15bbe89
castro2004 <jc300159@gmail.com>
2023-05-07 17:09:19 -0600

    CRUD reservacionController
    Se agrego el CRUD completo en el archivo de reservacionController.js

--- gestor_hoteles/src/controller/reservationController.js
+++ gestor_hoteles/src/controller/reservationController.js
@@ -5,8 +5,6 @@ const Reservation = require('../model/reservationModel')
 const Room = require('../model/roomModel')
 const Service = require('../model/serviceModel')
 
-//-----------------------------------crud Hoteles------------------------------------------
-
 //-------------------------------------create--------------------------------------------------
 
 const createReservation = async(req, res) => {
@@ -100,19 +98,128 @@ const readReaservation = async(req, res) => {
     }
 }
 
+//------------------------------------------------------------------update reservation------------------------------------------
+
+const updateReservation = async(req, res) => {
+
+    const {user, room, checkIn, checkOut, services, totalPrice, isActive} = req.body;
+    const {id} = req.body;
+    
+    try{
+
+        const reservation_pasado = await Reservation.findById(id);
+
+        if(!reservation_pasado.user){
+            const userAnterior = await User.findById(reservation_pasado)
+            userAnterior.reservations = null
+            await userAnterior.save();
+        }
+
+        const userExist = await User.findById(user);
+
+        if(!userExist){
+            return res.status(410).json({
+                msg: `El usuario no se encuestra existente`,
+                msg: `Verificar los datos del usuario`
+            });
+        }
+
+        const roomExist =  await Room.findById(room);
+
+        if(!roomExist){
+            return res.status(410).json({
+                msg: `La habitacion no se encuentr existe`,
+                msg: `Verifica los datos de la habitacion`
+            });
+        }
+
+        const servicesExist = await Service.findById(services);
+
+        if(!servicesExist){
+            return res.status(410).json({
+                msg: `El servicio no se esntra existe`,
+                msg: `Revisar los datos del servicio`
+            });
+        }
+
+        const reservatiComplete = await Reservation.findByIdAndUpdate(
+            id,
+            {user, room, checkIn, checkOut, services, totalPrice, isActive},
+            {new: true}
+        );
+
+        userExist.reservations = reservatiComplete._id
+        await userExist.save();
+
+        roomExist.reservations = reservatiComplete._id
+        await roomExist.save();
+
+        servicesExist.reservations = reservatiComplete._id
+        await servicesExist.save();
+        
+        return res.status(200).json({
+            msg: `La reservacion se acualizo de forma exitosa`,
+            reservacion: reservatiComplete
+        });
+
+    }catch(err){
+        console.log(err);
+        re.status(500).json({
+            msg: 'Error al actualizar la reservacion :('
+        });
+        throw new Error(err);
+    }
+
+}
+
 //--------------------------------------------------------------Delete reservation-----------------------------------------------
 
 const deleteReservation = async(req, res) => {
 
+    const {id, name} = req.params;
+    
     try{
 
+        const reservation = await Reservation.findById(id);
+        if(!reservation){
+            return res.status(404).json({
+                msg: `La reservacion que deseas eliminar no existe`,
+                msg:   `Verificar los datos escritos`
+            });
+        }
 
+        await reservation.remove();
+
+        const user = await User.findOne({reservation: id});
+
+        if(user){
+            user.reservations = null 
+            await user.save();
+        }
+
+        const room = await Room.findOne({room: id});
+
+        if(room){
+            room.reservations = null
+            await room.save();
+        }
+
+        const service = await Service.findOne({service: id});
+
+        if(service){
+            service.reservations = null
+            await service.save();
+        }
+
+        return res.json({
+            msg: `Su reservacion a sido eliminada o cancelada de forma existosa`
+        });
 
     }catch(err){
         console.log(err)
         res.status(510).send({
             ok: false,
-            msg: `No se podido crear la reservacion`,
+            msg: `No se podido eliminar o cancelar la reservacion`,
             error: err
         });
 
@@ -122,7 +229,10 @@ const deleteReservation = async(req, res) => {
 //---------------------------------------------exportaciones de las funciones-----------------------------------------------------
 
 module.exports = {
-    createReservation
+    createReservation,
+    readReaservation,
+    updateReservation,
+    deleteReservation
 }
 
 
