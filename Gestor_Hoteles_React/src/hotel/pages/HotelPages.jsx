e701d3a8f2cfea3de31da37b05c01222dfb33e34
ahernandez-2018417 <108430859+Lizardoahoxd@users.noreply.github.com>
2023-05-15 20:19:13 -0600

    Se hicieron algunos cambios, ya se puede cambiar entre paginas

---
+++ Gestor_Hoteles_React/src/hotel/pages/HotelPages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HarmonyHouse.css'
+
+export const HotelPages = () => {
+  return (
+
+    <div className="slider_conteiner">
+        <div className="slider position">
+        </div>
+    </div>
+ 
+  );
+}
--- Gestor_Hoteles_React/src/main.jsx
+++ Gestor_Hoteles_React/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MenuHotel from './MenuHotel'
-import Hotel from './hotel/components/Hotel'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { HarmonyHouse } from './HarmonyHouse';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hotel/>
-    {/* <MenuHotel/> */}
+    <BrowserRouter>
+      <HarmonyHouse/>
+    </BrowserRouter>
   </React.StrictMode>
 )
