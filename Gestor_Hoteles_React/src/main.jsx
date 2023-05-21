6a87b86baa9ab7ad60f3301fa59b4b7321c11ca8
ahernandez-2018417 <108430859+Lizardoahoxd@users.noreply.github.com>
2023-05-20 18:20:34 -0600

    .
    .

--- Gestor_Hoteles_React/src/main.jsx
+++ Gestor_Hoteles_React/src/main.jsx
@@ -1,18 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-=======
 import { BrowserRouter } from 'react-router-dom'
 import { HarmonyHouse } from './HarmonyHouse';
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,5 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <HarmonyHouse/>
     </BrowserRouter>
   </React.StrictMode>
->>>>>>> Ahernandez-2018417
 )
